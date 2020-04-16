d1f89909248eb039188374a63e207620593b20ca
Joe McCartin <kinelarty101@gmail.com>
2020-04-16 17:37:33 -0400

    initial commit of django news website

---
+++ users/models.py
@@ -0,0 +1,10 @@
+# users/models.py
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
